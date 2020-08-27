601fb31227ff6da76dc9c98f61544b0d03789af0
Armando Rodriguez <arod1207@me.com>
2020-08-27 11:18:26 -0500

    created procfile

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
+
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+
+app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
