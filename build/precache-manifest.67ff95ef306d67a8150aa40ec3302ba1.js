ad0051fd7a5d8a8388bbd2ad80a2452e600de899
Armando Rodriguez <arod1207@me.com>
2020-08-27 10:15:23 -0500

    ran npm build

---
+++ build/precache-manifest.67ff95ef306d67a8150aa40ec3302ba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa64e49cdeb4b584782b4301c0df2e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa491b7a74c87414145",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fa85fb888a01f1995697",
+    "url": "/static/css/main.1fb35cd9.chunk.css"
+  },
+  {
+    "revision": "dfa491b7a74c87414145",
+    "url": "/static/js/2.fa92e8e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa92e8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa85fb888a01f1995697",
+    "url": "/static/js/main.2f55ee45.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
