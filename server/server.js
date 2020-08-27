b6ede197076ff9cf499b53dfe08fa9a4875da716
Armando Rodriguez <arod1207@me.com>
2020-08-27 10:44:48 -0500

    changed path to build

--- server/server.js
+++ server/server.js
@@ -1,18 +1,8 @@
-const path = require('path');
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
-const publicPath = path.join(__dirname, '..', 'public');
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is up and running on http://localhost:${PORT}`);
-});
+app.listen(9000);
