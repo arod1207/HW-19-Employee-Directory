161f3a53a7d91fb23caf152c9b31e3f8ec432c8d
Armando Rodriguez <arod1207@me.com>
2020-08-26 22:39:31 -0500

    fixed styling

--- employee-directory/src/components/Employees.jsx
+++ employee-directory/src/components/Employees.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
+import '../App.css';
 
+// brought in the props from Searchbar //
 function Employees(props) {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm">
-                    <img src={props.people.picture.large} alt="..." />
-                    <h3>
-                        {props.people.name.first} {props.people.name.last}
-                    </h3>
-                    <h4>
-                        Email: {props.people.email} <br />
-                        Age: {props.people.dob.age}
-                    </h4>
-                </div>
-            </div>
-            <hr></hr>
+        <div className="employee-card">
+            <img src={props.people.picture.large} alt="..." />
+            <h3>
+                {props.people.name.first} {props.people.name.last}
+            </h3>
+            <h5>
+                Email: {props.people.email} <br />
+                Phone: {props.people.phone}
+            </h5>
         </div>
     );
 }
--- employee-directory/src/components/SearchBar.jsx
+++ employee-directory/src/components/SearchBar.jsx
@@ -5,27 +5,43 @@ import '../App.css';
 import Employees from './Employees';
 
 function SearchBar() {
+    // setEmployees was used to get the data from the api and set the data in the employees state //
     const [employees, setEmployees] = useState([]);
+    // setSearch was started as a empty string and used run in the input to recieve the value of the input and store it in the search //
     const [search, setSearch] = useState('');
 
+    // useEffect was left with only the search  so the api call would only be made during a search //
     useEffect(() => {
         axios
-            .get('https://randomuser.me/api/?nat=us&results=10&seed=foobar')
+            .get('https://randomuser.me/api/?nat=us&results=20&seed=foobar')
             .then((res) => {
                 setEmployees(res.data.results);
             })
             .catch((err) => {
                 console.log(err);
             });
-    }, []);
+    }, [search]);
 
+    // setup a filter method to loop through the setState from employees and then set everything to lowercase, passed in the search from setState //
     const filter = employees.filter((employee) => {
         return employee.name.first.toLowerCase().includes(search.toLowerCase());
     });
 
+    // passed the filter variable into the map method then added props to render the Employee Component //
     const employeeComponent = filter.map((people) => (
         <Employees key={people.name.first} people={people} />
     ));
+
+    const alphaOrder = () =>
+        employees.sort(function (a, b) {
+            if (a.name.last < b.name.last) {
+                return -1;
+            }
+            if (a.name.last > b.name.last) {
+                return 1;
+            }
+            return 0;
+        });
 
     return (
         <div>
@@ -36,10 +52,16 @@ function SearchBar() {
                         className="form-input"
                         type="text"
                         placeholder="Search"
+                        // created an onChange event to handle the setSearch, then setSearch passes the value to search
                         onChange={(e) => setSearch(e.target.value)}
                     ></input>
                 </div>
+                <button className="btn btn-primary" onClick={alphaOrder}>
+                    Sort Names in Alpha Order
+                </button>
             </div>
+
+            {/* Past in the employeeComponent variable created with the map method  */}
             {employeeComponent}
         </div>
     );
