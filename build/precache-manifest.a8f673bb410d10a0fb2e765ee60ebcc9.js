f4bd846b9cc56da85cc9fdd3ef03b8165d0c5939
Armando Rodriguez <arod1207@me.com>
2020-08-26 23:39:37 -0500

    added an express server

---
+++ build/precache-manifest.a8f673bb410d10a0fb2e765ee60ebcc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d44762df726a58adf4a2e9737b8a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f26a8d47b5ea89bebc18",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "341fdabbec056feb4317",
+    "url": "/static/css/main.56414cde.chunk.css"
+  },
+  {
+    "revision": "f26a8d47b5ea89bebc18",
+    "url": "/static/js/2.ec615e33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec615e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341fdabbec056feb4317",
+    "url": "/static/js/main.97959c6d.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
